a284ddfe5e9b7e83332d091b5bb3cc34c54aa7c1
Shivangi Reja <shreja@microsoft.com>
2019-03-06 15:56:06 -0800

    Added check for ServiceCommunicationError

--- lib/retry.ts
+++ lib/retry.ts
@@ -138,8 +138,8 @@ export async function retry<T>(config: RetryConfig<T>): Promise<T> {
         err = translate(err);
       }
       const isConnected = await checkNetworkConnection();
-      if (!isConnected && !err.retryable) {
-        err.name = "ConnectionDetachError";
+      if (!isConnected && !err.retryable && err.name === "ServiceCommunicationError") {
+        err.name = "ConnectionLostError";
         err.retryable = true;
       }
       lastError = err;
